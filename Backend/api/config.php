47a5644883d91da1efe53b2640c31af65c996202
njm-solus <njm.solus@gmail.com>
2026-06-23 11:10:58 +0300

    edits

--- Backend/api/config.php
+++ Backend/api/config.php
@@ -3,7 +3,7 @@
     $host = 'localhost';
     $dbname = 'backtoyou';
     $username = 'root';
-    $password = '';
+    $password = 'mariadb';
 
     try{
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
