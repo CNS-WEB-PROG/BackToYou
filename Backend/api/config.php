dd976057b29a152a7dfd86301bf5bf21c45a3dd5
njm-solus <njm.solus@gmail.com>
2026-06-23 08:25:05 +0300

    fixed line 9

--- Backend/api/config.php
+++ Backend/api/config.php
@@ -6,7 +6,7 @@
     $password = '';
 
     try{
-        $pdo = new PDO("mysql:host=$host;dbname;charset=utf8mb4", $username, $password);
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch (PDOException $e) {
         die("Database connection failed: " . $e->getMessage());
